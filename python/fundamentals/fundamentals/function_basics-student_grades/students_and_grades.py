4779fce2d59b7fded865b2c3bf7eb315d47e8297
KNJour <KNJournell@gmail.com>
2021-04-07 10:00:15 -0400

    students and grades

--- python/fundamentals/fundamentals/function_basics-student_grades/students_and_grades.py
+++ python/fundamentals/fundamentals/function_basics-student_grades/students_and_grades.py
@@ -1,3 +1,33 @@
-fullList = {'Student': 'John', 'Course': 'Math', 'Grade': '88'}
+# fullList = {'Student': 'John', 'Course': 'Math', 'Grade': '88'}
 
-print(fullList)
+# studentNum = input('How Many Students Do You Have?')
+
+def studentInput():
+    studentNum = input('How Many Students Do You Have?')
+    studentList = []
+    studentStr = ""
+    for i in range(0, int(studentNum)):
+        x = input("Student Name:")
+        y = input("Student Grade")
+        z = input("Select a course 1 - MATH, 2 - SCIENCE, 3 - HISTORY")
+        if z == "1":
+            z = "MATH"
+        elif z == "2":
+            z = "SCIENCE"
+        elif z == "3":
+            z = "HISTORY"
+        studentList.append([f"Name: {x} Grade: {y} Class {z}"])
+        studentStr += str(', '.join(studentList[i])) 
+        studentStr += " || "
+        # globals()[f"tempvar{i}"] = f"Name: {x} Grade: {y} Class {z}"
+        # globals()[f"tempvar{i}"] = str(', '.join(studentList[i]))
+        
+    print(studentStr)
+    # for k in range(0, int(studentNum)):
+    #     print(f"tempvar{k}")
+studentInput()
+     
+
+    #  for i in range(0, int(studentnum)):
+
+    #      studentList[0].append(input())
