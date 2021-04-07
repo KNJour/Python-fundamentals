beda066b270739a54cb5f1c2bd19fb8010fb86ed
KNJour <KNJournell@gmail.com>
2021-04-07 15:40:26 -0400

    User.py completed

--- python/fundamentals/oop/User.py
+++ python/fundamentals/oop/User.py
@@ -14,14 +14,32 @@ class User:
     def display_user_balance(self):
         print(self.account_balance)
 
+    def transfer_money(self,target,amount):
+        self.make_withdrawal(amount)
+        target.make_deposit(amount)
+        print(f"your balance is now {self.account_balance}")
+        print(f"their balance is now {target.account_balance}")
+
 guido = User("guido", "gg@code.com")
 keith = User("Keith", "KN@gmail.com")
 bob = User("Bob", "Bob@bob.bob")
 
 guido.make_deposit(100)
-print(guido.account_balance)
-
+guido.make_deposit(50)
+guido.make_deposit(250)
 guido.make_withdrawal(50)
-print(guido.account_balance)
+guido.display_user_balance()
 
-guido.dispay_user_balance(guido)
+keith.make_deposit(100)
+keith.make_deposit(300)
+keith.make_withdrawal(200)
+keith.make_withdrawal(150)
+keith.display_user_balance()
+
+bob.make_deposit(100)
+bob.make_withdrawal(300)
+bob.make_withdrawal(200)
+bob.make_withdrawal(150)
+bob.display_user_balance()
+
+guido.transfer_money(keith, 100)
